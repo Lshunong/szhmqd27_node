b00cacb4c4b75a4ec56fd3df4f376b9df4a03875
Your Name <you@example.com>
2019-01-21 08:35:14 +0800

    2-1完成注册页面和获取登录页面

--- src/app.js
+++ src/app.js
@@ -1,16 +1,32 @@
 //导包
 const express = require('express')
 const path = require('path')
-
+var bodyParser = require('body-parser')
+var session = require('express-session')
 
 //创建app
 const app = express()
+
+// parse application/x-www-form-urlencoded 解析url
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json 解析json
+app.use(bodyParser.json())
+
+// Use the session middleware 解析数字验证码图片路径 放入session
+app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 600000 }}))
+
+//进行登录请求判断
+
+
 //设置静态资源根目录
 app.use(express.static(path.join(__dirname,'public')))
 
 
-//导入路由对象
+
+//导入路由对象 路由中间件写在最后
 const accountRouter =require(path.join(__dirname,'./routers/accountRouter.js'))
+//一级路径account
 app.use('/account',accountRouter)
 
 //启动
--- src/routers/accountRouter.js
+++ src/routers/accountRouter.js
@@ -11,8 +11,16 @@ const accountRouter = express.Router()
 //导入控制器模块
 const accountController = require(path.join(__dirname,'../controllers/accountController'))
 
-//MVC
+//MVC 获取注册页面
 accountRouter.get('/register',accountController.getRegisterPage)
+//处理注册
+accountRouter.post('/register',accountController.register)
+//获取登录页面
+accountRouter.get('/login',accountController.getLoginPage)
+//获取验证码
+accountRouter.get('/vcode',accountController.getVcodeImage)
+//处理登录请求
+accountRouter.post('/login',accountController.login)
 
 //导出路由对象
 module.exports =accountRouter
